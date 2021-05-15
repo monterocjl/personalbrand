cceb2bcf59d2a211de98c1fc85cb639e1d69ab14
Juan Montero <81704162+monterocjl@users.noreply.github.com>
2021-05-15 18:07:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,3 +7,49 @@ addEventListener('DOMContentLoaded', () => {
         })
     }
 })
+
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.carousel__lista'), {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.carousel__indicadores',
+        arrows: {
+          prev: '.carousel__anterior',
+          next: '.carousel__siguiente'
+       },
+
+       responsive: [
+        {
+
+          breakpoint: 700,
+          settings: {
+            // Set to `auto` and provide item width to adjust to viewport
+            slidesToShow: '2',
+            slidesToScroll: '2',
+            itemWidth: 150,
+            duration: 0.25
+          }
+        },{
+
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            itemWidth: 150,
+            duration: 0.25
+          }
+        },{
+
+            breakpoint: 1130,
+            settings: {
+              slidesToShow: 4,
+              slidesToScroll: 4 ,
+              itemWidth: 150,
+              duration: 0.25
+            }
+          }
+      ]   
+    });
+});
